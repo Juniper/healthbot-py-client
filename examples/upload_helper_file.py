0173611e88c3b91c54206c01558f677d66001a74
Sharanya Bhat <rsharanyabhat@gmail.com>
2022-01-28 08:16:42 +0530

    Adding comment

--- examples/upload_helper_file.py
+++ examples/upload_helper_file.py
@@ -6,6 +6,7 @@ gui_password = 'pwd'
 
 
 with HealthBotClient(ip, gui_username, gui_password, port=8080) as hb:
+    # We can upload rules/playbooks/udf python files (any kind of helper file) 
     file = "/<path>/myrule.rule"
     hb.rule.upload_rule_file(file)
     hb.commit()
